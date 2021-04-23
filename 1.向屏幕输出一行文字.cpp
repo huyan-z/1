1314fef0e9b5ee55e7d090da9c28b0f3938850bd
huyan-z <1079271059@qq.com>
2021-04-23 16:20:36 +0800

    向屏幕输出一行文字（演示）

---
+++ 1.向屏幕输出一行文字.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>					//stdio.h通知编译器printf函数的功能是什么，后面才可以使用printf函数
+int main(void)						//任何程序都是从main函数开始执行，从main函数退出执行的
+{									//花括号包裹的程序语句称为复合语句
+	printf("haha\n");				//printf的功能是输出双撇号中的内容，双撇号中的内容称之为字符串
+	return 0;						//分号结尾的是 程序的程序语句
+}
